34be1901fb410fc35b70edd511262dfec70d3c30
ginodest <ginodest71@gmail.com>
2023-06-29 09:14:51 -0300

    Cambios en los calculos

--- js/Calculos.js
+++ js/Calculos.js
@@ -1,52 +1,41 @@
-alert("hola")
-// definimos la clase del objeto..
-class filamento {
-    constructor(nombre,precioKg){
-        this.nombre = nombre;
-        this.precioKg = precioKg
-    }
-}
-let filamento1 = new filamento("pla",4000);
-let filamento2 = new filamento("abs",4650);
-let filamento3 = new filamento("petg",4270);
-let filamento4 = new filamento("tpu",4120);
-
-//array que contiene los objetos
-let filamentos = [filamento1,filamento2,filamento3,filamento4]
-
+//aca tenemos los objetos en una lista
+var filamentos = [
+    { nombre: "pla", precioKg:4000},
+    { nombre: "abs", precioKg:4650},
+    { nombre: "petg", precioKg:4270},
+    { nombre: "tpu", precioKg:4120}
+];
 // creamos la funcion para los calculos
-let condicion = prompt("Desea realizar los calculos? (si/no)").toLocaleLowerCase();
-
-function coste(filamento){
+function coste(){
+    //tomamos como un gasto fijo que en electrecidad tenemos un consumo de 28.05 kw/H
+    let costesLuz = 28.05;
+    //tomamos como consumo medio de la impresora un 0.50 kw
+    let consumoMedio = 0.50;
+    //entradas
+    let tipo = prompt("ingrese el tipo de filamento").toLowerCase();
     let peso = prompt("ingrese el Peso de la pieza en KG");
-    let filamento = prompt("ingrese el tipo de filamento (0 PLA - 1 ABS - 2 PETG - 3 TPU)");
     let tiempo = prompt("ingrese el tiempo de impresion en Horas");
     
-    if (filamento === 0){
-        costeTotal = filamentos[0].precioKg*peso/1000 + tiempo*costesLuz*consumoMedio;
-    } else if(filamento === "abs"){
-        costeTotal = filamentos[1].precioKg*peso/1000 + tiempo*costesLuz*consumoMedio;
-    }
-    else if(filamento === "petg"){
-        costeTotal = filamentos[2].precioKg*peso/1000 + tiempo*costesLuz*consumoMedio;
-    }
-    else if(filamento === "tpu"){
-        costeTotal = filamentos[3].precioKg*peso/1000 + tiempo*costesLuz*consumoMedio;
+
+    if (tipo === filamentos[0].nombre){
+        costeTotal = parseInt(filamentos[0].precioKg)*parseFloat(peso)/1000 + parseFloat(tiempo)*costesLuz*consumoMedio;
+        alert("PESO: "+ peso + "\nTIEMPO: "+ tiempo + "\nMATERIAL: "+filamentos[0].nombre+"\nCoste de impresion: " +"$"+ parseFloat(costeTotal) + " pesos");
+    } else if (tipo === filamentos[1].nombre) {
+        costeTotal = parseInt(filamentos[1].precioKg)*parseFloat(peso)/1000 + parseFloat(tiempo)*costesLuz*consumoMedio;
+        alert("PESO: "+ peso + "\nTIEMPO: "+ tiempo + "\nMATERIAL: "+filamentos[1].nombre+"\nCoste de impresion: " +"$"+ parseFloat(costeTotal) + " pesos");
+    } else if (tipo === filamentos[2].nombre) {
+        costeTotal = parseInt(filamentos[2].precioKg)*parseFloat(peso)/1000 + parseFloat(tiempo)*costesLuz*consumoMedio;
+        alert("PESO: "+ peso + "\nTIEMPO: "+ tiempo + "\nMATERIAL: "+filamentos[2].nombre+"\nCoste de impresion: " +"$"+ parseFloat(costeTotal) + " pesos");
+    } else if(tipo === filamentos[3].nombre){
+        costeTotal = parseInt(filamentos[3].precioKg)*parseFloat(peso)/1000 + parseFloat(tiempo)*costesLuz*consumoMedio;
+        alert("PESO: "+ peso + "\nTIEMPO: "+ tiempo + "\nMATERIAL: "+filamentos[3].nombre+"\nCoste de impresion: " +"$"+ parseFloat(costeTotal) + " pesos");
     }
 }
 
-    while (condicion != 'no'){
-        coste(filamento);
-        alert("costo total de la impresion es de: " +"$"+ costeTotal + " pesos");
-        condicion = prompt("Desea realizar Otro calculo? (si/no)").toLocaleLowerCase();
-    }
-    
-
-//                       ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇ codigo viejo ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
-
-//Declaramos algunas variables para usarlas luego
-
-//tomamos como un gasto fijo que en electrecidad tenemos un consumo de 26.05 kw/H
-let costesLuz = 26.05;
-//tomamos como consumo medio de la impresora un 0.50 kw
-let consumoMedio = 0.50
+//bucle.
+//tomé en cuenta en la entrega anterior del uso de los tolowercase!
+condicion = prompt("Desea calcular los costos? (SI/NO)").toLowerCase();
+while (condicion != 'no'){
+    coste();
+    condicion = prompt("Desea calcular OTRA VEZ? (SI/NO)").toLowerCase();
+}
